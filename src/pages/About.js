06efce09e482156254217fa1b57d9ef3f0d6b9f4
codeshootah <grigorycode@gmail.com>
2019-08-15 13:33:49 +0300

    Add react-router and pages

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, itaque.</p>
+  </div>
+)
